1d577d668a4537d1937e3e16c449e9c26f1779b0
Marco <marco.chianese93@gmail.com>
2020-12-11 16:17:43 +0100

    implementazione parziale jumbotron

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/script.js', 'public/').sass('src/style.scss', 'public/');
+mix.js('src/script.js', 'public/').sass('src/style.scss', 'public/').options({ processCssUrls : false });
 
 // Full API
 // mix.js(src, output);
